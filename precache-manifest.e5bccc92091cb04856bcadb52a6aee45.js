8189d02b8cc7f7b50413951a080423676fcc40aa
Vineet <vineet.agarwal@oracle.com>
2019-07-19 10:14:59 -0600

    Updates

---
+++ precache-manifest.e5bccc92091cb04856bcadb52a6aee45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9db33fc73b6112e9b880946fdafe4b5",
+    "url": "/vineetagarwal208/vineet-portfolio/index.html"
+  },
+  {
+    "revision": "23ec4a3e7022768e3c11",
+    "url": "/vineetagarwal208/vineet-portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/vineetagarwal208/vineet-portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "23ec4a3e7022768e3c11",
+    "url": "/vineetagarwal208/vineet-portfolio/static/js/main.c93e010d.chunk.js"
+  },
+  {
+    "revision": "00ccc3378412a3966414",
+    "url": "/vineetagarwal208/vineet-portfolio/static/js/runtime~main.797125dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vineetagarwal208/vineet-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
